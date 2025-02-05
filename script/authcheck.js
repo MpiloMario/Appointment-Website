db9f9aad898507920b518bf28e9204a56123c840
Mpilo <mpilomastermind@gmail.com>
2025-02-05 21:28:39 +0200

    Update authcheck.js

--- script/authcheck.js
+++ script/authcheck.js
@@ -1,6 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-    if (!sessionStorage.getItem("name")) {
-        alert("Please log in to access this page.");
-       window.location.href = "user.html";
+document.addEventListener("DOMContentLoaded", () => {
+    const userToken = localStorage.getItem("userToken");
+
+    if (!userToken) {
+        console.warn("No user token found! Redirecting to login...");
+        window.location.href = "user.html"; // Redirect to login
+    } else {
+        console.log("User is authenticated, access granted.");
     }
 });
